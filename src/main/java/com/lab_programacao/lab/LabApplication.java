77fcd15cc03a5a28a8c02377ba04b76c406a0c02
José Eduardo de S. Oliveira <eduardo.jeso@gmail.com>
2020-09-22 16:43:44 -0300

    first commit

---
+++ src/main/java/com/lab_programacao/lab/LabApplication.java
@@ -0,0 +1,13 @@
+package com.lab_programacao.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabApplication.class, args);
+	}
+
+}
