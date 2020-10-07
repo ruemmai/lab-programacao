16970193503ac20e56af828d9b6d03320e8a6aa1
José Eduardo de S. Oliveira <eduardo.jeso@gmail.com>
2020-10-07 10:28:07 -0300

    novo metodo vender mecadoria na classe de serviço VendasService

--- src/main/java/com/lab_programacao/lab/services/VendasServices.java
+++ src/main/java/com/lab_programacao/lab/services/VendasServices.java
@@ -6,23 +6,48 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.List;
 
+import com.lab_programacao.lab.models.entities.Mercadorias;
+import com.lab_programacao.lab.models.entities.VendaMercadoria;
 import com.lab_programacao.lab.models.entities.Vendas;
+import com.lab_programacao.lab.models.repositories.MercadoriasRepository;
+import com.lab_programacao.lab.models.repositories.VendaMercadoriaRepository;
 import com.lab_programacao.lab.models.repositories.VendasRepository;
 
 @Service
 public class VendasServices {
 
   @Autowired
-  VendasRepository repository;
+  VendasRepository vendasRepository;
+
+  @Autowired
+  MercadoriasRepository mercadoriasRepository;
+
+  @Autowired
+  VendaMercadoriaRepository vendaMercadoriaRepositiry;
 
   public Vendas salvar(Vendas venda) {
-    Vendas salvo = repository.save(venda);
+    Vendas salvo = vendasRepository.save(venda);
     return salvo;
   }
 
   public List<Vendas> obterVendasPorData(Date data) {
     List<Vendas> vendasPorData;
-    vendasPorData = repository.findVendasByData(data);
+    vendasPorData = vendasRepository.findVendasByData(data);
     return vendasPorData;
+  }
+
+  public Vendas venderMecadoria(Mercadorias mercadorias) {
+    Vendas novaVenda = Vendas.builder().dataVenda(new Date()).build();
+    vendasRepository.save(novaVenda);
+
+    Mercadorias mercadoriaSalva;
+    mercadoriaSalva = mercadoriasRepository.save(mercadorias);
+
+    VendaMercadoria novaVendaMercadoria = VendaMercadoria.builder().venda(Long.valueOf(novaVenda.getId()))
+        .mercadoria(Long.valueOf(mercadoriaSalva.getId())).build();
+
+    vendaMercadoriaRepositiry.save(novaVendaMercadoria);
+
+    return novaVenda;
   }
 }
